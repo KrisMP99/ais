5a30566d0980fa92dab2e254f8b834fabcaf1a2c
KrisMP99 <kpede19@student.aau.dk>
2022-03-08 12:17:46 +0100

    WIP on cleansing

--- backend/app/data_cleansing.py
+++ backend/app/data_cleansing.py
@@ -7,6 +7,7 @@ from sqlalchemy import create_engine
 
 ERROR_LOG_FILE_PATH = "C:\\Users\\Kristian\\Desktop\\error_log.txt"
 
+COLUMNS = ['timestamp', 'mobile_type', 'mmsi', 'latitude', 'longitude', 'navigational_status', 'rot', 'sog', 'cog', 'heading', 'imo', 'callsign', 'name', 'ship_type', 'cargo_type', 'width', 'length', 'type_of_position_fixing_device', 'draught', 'destination', 'eta', 'data_source_type']
 
 load_dotenv()
 USER = os.getenv('POSTGRES_USER')
@@ -15,7 +16,19 @@ HOST_DB = os.getenv('HOST_DB')
 DB_NAME = os.getenv('DB_NAME')
 
 # chunk size to load in (we do not have enough memory to load in everything at once)
-chunk_size = 1000000
+CHUNK_SIZE = 1000000
+
+class Point:
+    def __init__(self, longitude, latitude, sog, timestamp, mmsi):
+        self.longitude = longitude
+        self.latitude = latitude
+        self.sog = sog
+        self.timestamp = timestamp
+        self.mmsi = mmsi
+    
+    # Manhatten distance because it's fast af
+    def distance(self, p):
+        return abs((self.longitude - p.longitude) + (self.latitude - p.latitude))
 
 # Logging for file
 def get_logger():
@@ -39,7 +52,7 @@ engine = create_engine(db_string)
 
 
 sql_query = "SELECT " \
-            "(timestamp, mobile_type, mmsi, latitude, longitude, navigational_status, rot, sog, cog, heading, imo, callsign, name, ship_type, cargo_Type, width, length, type_of_position_fixing_device, draught, destination, eta, data_source_type) " \
+            "timestamp, mobile_type, mmsi, latitude, longitude, navigational_status, rot, sog, cog, heading, imo, callsign, name, ship_type, cargo_type, width, length, type_of_position_fixing_device, draught, destination, eta, data_source_type " \
             "FROM raw_data " \
             "WHERE "\
                 "(mobile_type = 'Class A') AND "\
@@ -47,10 +60,18 @@ sql_query = "SELECT " \
                 "(longitude >= 3.2 AND longitude <= 16.5) AND "\
                 "(heading >= 0 AND heading <= 359) AND "\
                 "(rot >=0 AND rot <= 720) AND" \
-                "(sog >= 0 AND sog <= 102)"
+                "(sog >= 0 AND sog <= 102) " \
+                "LIMIT 100000"
 
-for data in pd.read_sql_query(sql_query, engine, chunksize=10):
-    quit()
+            
+for sql in psql.read_sql_query(sql=sql_query, con=engine, chunksize=CHUNK_SIZE):
+    df = pd.DataFrame(sql, columns=COLUMNS)
+    
+
+
+    print(df['mmsi'].drop_duplicates())
+
+
 
 
 logger.info("Done!")

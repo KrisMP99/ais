286548073f1ce012a56de2e3392e2d3dea164467
Cecilie <cfog19@student.aau.dk>
2022-03-10 10:38:45 +0100

    updated main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ from fastapi.encoders import jsonable_encoder
 USER = os.getenv('POSTGRES_USER')
 PASS = os.getenv('POSTGRES_PASSWORD')
 
-SQLALCHEMY_DATABASE_URL = "postgresql://ais:a401@db/aisdb"
+SQLALCHEMY_DATABASE_URL = "postgresql://{USER}:{PASS}@db/aisdb"
 engine = create_engine( SQLALCHEMY_DATABASE_URL, convert_unicode=True )
 
 Session = sessionmaker(bind=engine)
@@ -27,15 +27,15 @@ async def root():
 
 @app.get("/map_bounds")
 async def get_mapBounds():
-    query = "SELECT ST_AsGeoJson(hexes.geom)::geographi \
-                FROM ST_HexagonGrid(\
-                    0.5,\
-                    ST_SetSRID(\
-                        ST_EstimatedExtent('map_bounds','geom'), 3857\
-                    )\
-                ) AS hexes INNER JOIN\
-                    map_bounds AS mb ON ST_Intersects(mb.geom, hexes.geom) \
-            GROUP BY hexes.geom;"
+    query = "WITH geometry_hexagons \
+        AS (SELECT hexes.geom  \
+            FROM ST_SquareGrid\
+                (0.5, ST_SetSRID(ST_EstimatedExtent('map_bounds','geom'), 4326))\
+                AS hexes INNER JOIN map_bounds AS mb \
+                ON ST_Intersects(mb.geom, ST_Transform(hexes.geom, 4326)) \
+            GROUP BY hexes.geom)\
+        SELECT ST_AsGeoJson(gh.geom::Geography) \
+        FROM geometry_hexagons AS gh;"
     sql = psql.read_sql_query(sql=query, con=engine)
     df = pd.DataFrame(sql)
     return df

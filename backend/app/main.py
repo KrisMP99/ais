af3bebd90f4c2dd886b78b31e57cf1ceef92fd00
Cecilie <cfog19@student.aau.dk>
2022-03-15 14:20:18 +0100

    fixed env

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,8 +29,8 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://{TESTSERVER}",
-    "http://{PRODSERVER}"
+    f"http://{TESTSERVER}",
+    f"http://{PRODSERVER}"
 ]
 app.add_middleware(
     CORSMiddleware,

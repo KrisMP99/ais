4bc07d1175d0193d7d4f967a18c18c24449a5a1b
Cecilie <cfog19@student.aau.dk>
2022-03-15 12:13:40 +0100

    tried fixing env

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,8 +29,8 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    {TESTSERVER},
-    {PRODSERVER}
+    "http://{TESTSERVER}",
+    "http://{PRODSERVER}"
 ]
 app.add_middleware(
     CORSMiddleware,

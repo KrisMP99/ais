f8ab07c3765ad8243397552e3cf9039bc6c49695
Cecilie <cfog19@student.aau.dk>
2022-03-17 11:43:40 +0100

    updated API for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import LeafletMap from './Components/LeafletMap';
 import DKMap from './Components/Map';
 import './Leaflet.css';
+console.log(process.env.REACT_APP_API_KEY)
 
 function App() {
   return (

23ded100563530ff2251a7294ad32c1a9f5e88b7
KrisMP99 <kpede19@student.aau.dk>
2022-03-17 12:16:47 +0100

    WIP - Adding douglas algorithm

--- backend/app/douglas_peucker.py
+++ backend/app/douglas_peucker.py
@@ -1,8 +1,31 @@
 import pandas as pd, numpy as np
 import matplotlib.pyplot as plt
 from shapely.geometry import LineString
+from dotenv import load_dotenv
 from time import time
 import data_cleansing as dc
+import psycopg2
+import os
+
+USER = os.getenv('POSTGRES_USER')
+PASS = os.getenv('POSTGRES_PASSWORD')
+
+def insert_line_strings(mmsi_list, linestring_list):
+    sql = "INSERT INTO geometries(mmsi, linestring) VALUES(%s, %s)"
+    conn = None
+
+    try:
+        conn = psycopg2.connect(database="aisdb", user=USER, password=PASS, host="localhost", port="5432")
+        cursor = conn.cursor()
+        cursor.executemany(sql, mmsi_list, linestring_list)
+        conn.commit()
+        cursor.close()
+    except(Exception, psycopg2.DatabaseError) as err:
+        print(err)
+    finally:
+        if conn is not None:
+            conn.close()
+
 
 if __name__ == '__main__':
     # Loading of the point data from the csv file
@@ -17,43 +40,51 @@ if __name__ == '__main__':
                 "(mmsi IS NOT NULL) " \
                 "ORDER BY timestamp ASC ")
 
-    tripList = dc.get_trips(df)
+    trip_list = dc.get_trips(df)
 
-    tripList = dc.get_partitioned_trips(tripList)
+    trip_list = dc.partition_trips(trip_list)
 
-    dc.remove_outliers(tripList)
+    trip_list = dc.remove_outliers(trip_list)
 
-    for trip in tripList: 
+    total_linestrings = []
+    mmsi_list = []
+
+    for trip in trip_list: 
         pointList = trip.get_points_in_trip()
         data = []
         for point in pointList:
             data.append([point.latitude, point.longitude])
         
-        df = pd.DataFrame(data,colums=['latitude','longitude'])
+        df = pd.DataFrame(data, columns=['latitude','longitude'])
         
         coordinates = df[["latitude", "longitude"]].values
 
         line = LineString(coordinates)
 
-        tolerance = 0.15
+        tolerance = 0.0015
 
         simplified_line = line.simplify(tolerance, preserve_topology=False)
 
-        print(line.length, 'line length')
-        print(simplified_line.length, 'simplified line length')
+        total_linestrings.append(simplified_line)
+        mmsi_list.append(trip.get_mmsi())
+        
+
+        # print(line.length, 'line length')
+        # print(simplified_line.length, 'simplified line length')
         print(len(line.coords), 'coordinate pairs in full data set')
         print(len(simplified_line.coords), 'coordinate pairs in simplified data set')
         print(round(((1 - float(len(simplified_line.coords)) / float(len(line.coords))) * 100), 1), 'percent compressed')
-        quit()
+        
 
+        # lon = pd.Series(pd.Series(simplified_line.coords.xy)[1])
+        # lat = pd.Series(pd.Series(simplified_line.coords.xy)[0])
+        # si = pd.DataFrame({'latitude': lat, 'longitude': lon})
+        # si.tail()
+        # si.to_csv('newCSVFile')
+        # print(si)
+    
+    insert_line_strings(mmsi_list, total_linestrings)
 
-
-    lon = pd.Series(pd.Series(simplified_line.coords.xy)[1])
-    lat = pd.Series(pd.Series(simplified_line.coords.xy)[0])
-    si = pd.DataFrame({'latitude': lat, 'longitude': lon})
-    si.tail()
-    si.to_csv('newCSVFile')
-    print(si)
 
 # https://geoffboeing.com/2014/08/reducing-spatial-data-set-size-with-douglas-peucker/
 # https://github.com/gboeing/2014-summer-travels

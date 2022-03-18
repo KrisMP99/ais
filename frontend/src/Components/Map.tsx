175d0b0d94598ea69a33cfbca31b0b22ad81a66b
Alex Farup Christensen <56341143+AlexFarup@users.noreply.github.com>
2022-03-18 14:08:04 +0100

    Polyline vizualizer

--- frontend/src/Components/Map.tsx
+++ frontend/src/Components/Map.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Map.css';
 import Hexagon from './Hexagon'
-import { MapContainer, TileLayer, Marker, Polygon, Polyline } from 'react-leaflet';    
+import { MapContainer, TileLayer, Marker, Polyline } from 'react-leaflet';    
 import '../Leaflet.css';
-import L, { LatLngBoundsExpression, LatLngExpression, LatLng, Icon } from 'leaflet';
+import L, { LatLngBoundsExpression, LatLngExpression, LatLng} from 'leaflet';
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-import * as testsss from "../test.json";
-import Helmet from 'react-helmet';
-import vizualizeData from "./newCSVFile.json"
+import vizualizeData from "./polyLineTester.json";
+import vizualizeData2 from "./polyLine2.json";
 
 
 interface DKMapProps {
@@ -23,7 +22,14 @@ const MAP_CENTER: LatLng = new LatLng(55.8581, 9.8476);
 const MAP_BOUNDS: LatLngBoundsExpression = [[58.5, 3.2], [53.5, 16.5]];
 const response = window.fetch('http://127.0.0.1:8008/map_bounds');
 
+
+
+
+
+
 console.log(response);
+
+
 
 export class DKMap extends React.Component<DKMapProps, DKMapStates> {
     
@@ -36,6 +42,8 @@ export class DKMap extends React.Component<DKMapProps, DKMapStates> {
             markers: null
         }
     }
+    
+    
 
 
     render() {
@@ -58,7 +66,33 @@ export class DKMap extends React.Component<DKMapProps, DKMapStates> {
         //       ))
         // }
 
-        
+        // Code to create a 2d array of the an JSON array of objects, this is needed because leaflet polyline component only takes 2d arays
+        var json = vizualizeData;
+        var done: any = [];
+        // https://stackoverflow.com/questions/30825129/how-to-convert-json-array-into-javascript-2d-array
+        json.forEach(function(object){
+            done.push([object.latitude, object.longitude]);
+        });
+
+        var json2 = vizualizeData2;
+        var done2: any = [];
+        // https://stackoverflow.com/questions/30825129/how-to-convert-json-array-into-javascript-2d-array
+        json2.forEach(function(object){
+            done2.push([object.latitude, object.longitude]);
+        });
+
+        //function jsonArrayTo2D(json: []) {
+          //  let header: [] = [],
+            //    AoA = [];
+            //json.forEach(obj => {
+             //   Object.keys(obj).forEach(key => header.includes(key) || header.push(key))
+              //  let thisRow = new Array(header.length);
+               // header.forEach((col, i) => thisRow[i] = obj[col] || '')
+               // AoA.push(thisRow);
+            //})
+           // AoA.unshift(header);
+           // return AoA;
+        //}
 
         return (
             <MapContainer
@@ -67,7 +101,7 @@ export class DKMap extends React.Component<DKMapProps, DKMapStates> {
                 bounds={MAP_BOUNDS}
                 zoom={7}
                 minZoom={7}
-                maxZoom={12}
+                maxZoom={16}
                 scrollWheelZoom={true}
                 onClick={(e: any) => this.addSingleMarker(e)}
             >
@@ -75,15 +109,38 @@ export class DKMap extends React.Component<DKMapProps, DKMapStates> {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     bounds={MAP_BOUNDS}
                 />
-
-                {vizualizeData.map(data => (
-                    <Marker
+                
+                {/* {vizualizeData.map(data => (
+                    <Polyline
                     key={data.FIELD1}
-                    position={[data.latitude, data.longitude]}>
-                    </Marker>
-                ))
+                    weight={2}
+                    positions={[[data.latitude, data.longitude]]}>
+                    </Polyline>
+                    
+                ))} */}
 
-                }
+                    <Polyline
+                    color='red'
+                    positions={done}>
+                    </Polyline>
+                    
+                    <Polyline
+                    color='green'
+                    positions={done2}>
+                    </Polyline>
+                    
+
+                {/* {vizualizeData.map(data2 => (
+                    <Marker
+                    key={data2.FIELD1}
+                    position={[data2.latitude, data2.longitude]}>
+                    
+                    </Marker>
+                    
+                ))} */}
+                
+                
+                
 
 
                 {/* <Hexagon center={MAP_CENTER} rad={600} /> */}

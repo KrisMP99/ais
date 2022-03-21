6b66bcdd0f15c9de7131c454629621005c71e000
Kristian <kpede19@student.aau.dk>
2022-03-21 21:09:00 +0100

    WIP

--- backend/app/data_insertion.py
+++ backend/app/data_insertion.py
@@ -1,4 +1,3 @@
-from tarfile import POSIX_MAGIC
 from venv import create
 import pandas as pd, numpy as np
 import matplotlib.pyplot as plt
@@ -10,7 +9,9 @@ import os
 import pygrametl
 from pygrametl.datasources import SQLSource
 from pygrametl.tables import Dimension, FactTable
-import data_cleansing as dc
+import douglas_peucker as dp
+from sqlalchemy import create_engine
+
 
 load_dotenv()
 USER = os.getenv('POSTGRES_USER')
@@ -18,16 +19,21 @@ PASS = os.getenv('POSTGRES_PASSWORD')
 HOST_DB = os.getenv('HOST_DB')
 DB_NAME = os.getenv('DB_NAME')
 
-pgconn = psycopg2.connect(database="aisdb", user=USER, password=PASS, host="localhost", port="5432")
-pgconn_wrapper = pygrametl.ConnectionWrapper(connection=pgconn)
+db_string = f"postgresql://{USER}:{PASS}@{HOST_DB}/{DB_NAME}"
+engine = create_engine(db_string)
+# pgconn = psycopg2.connect(database="aisdb", user=USER, password=PASS, host="localhost", port="5432")
+# pgconn_wrapper = pygrametl.ConnectionWrapper(connection=pgconn)
 
-
+def insert_cleansed_data(df):
+    df.to_sql('cleansed', engine, if_exists='append', index=False)
 
 def convert_timestamp_to_time_and_date(timestamp):
     print(timestamp)
 
-for timestamp in zip(df.timestamp):
-    convert_timestamp_to_time_and_date(timestamp[0])
+df = dp.create_line_strings()
+insert_cleansed_data(df)
+
+
 
 
 

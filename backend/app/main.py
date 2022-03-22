2705e9a5ae5a54c7905ce65ad260a8326190bab4
Cecilie <cfog19@student.aau.dk>
2022-03-22 11:31:20 +0100

    cors v3

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,32 +1,30 @@
 import os
 from fastapi import FastAPI
 from dotenv import load_dotenv
-from starlette.middleware.cors import CORSMiddleware
-from starlette.middleware import Middleware
+from fastapi.middleware import Middleware
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import trips
 
 load_dotenv()
 TESTSERVER = os.getenv('TEST_SERVER')
 PRODSERVER = os.getenv('PRODUCTION_SERVER')
-ORIG = os.getenv('ORIGIN_ONE')
-ORIG2 = os.getenv('ORIGIN_TWO')
-
-app = FastAPI()
-
-app.include_router(trips.router)
 
 origins = [
-    '*',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     f'http://{TESTSERVER}:3000',
     f'http://{PRODSERVER}:3000'
 ]
 
-app = CORSMiddleware(
-    app=app,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["POST", "GET"],
-	allow_headers=["*"],
-)
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*']
+    )
+]
+
+app = FastAPI(middleware=middleware)
+app.include_router(trips.router)

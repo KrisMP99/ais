162004c201d19d7cdbdd7d92e72bf13244a608e2
Cecilie <cfog19@student.aau.dk>
2022-03-22 09:17:45 +0100

    switched latitude, longitude

--- backend/app/routers/trips.py
+++ backend/app/routers/trips.py
@@ -23,8 +23,8 @@ async def root():
 
 @router.post('/trip')
 async def get_trip(p1: Coordinate, p2: Coordinate): 
-    gp1 = Point((p1.lat, p1.long))
-    gp2 = Point((p2.lat, p2.long))
+    gp1 = Point((p1.long, p1.lat))
+    gp2 = Point((p2.long, p2.lat))
     query = f"WITH gp1 AS (\
         SELECT ST_AsText(ST_GeomFromGeoJSON('{gp1}')) As geom),\
         gp2 AS (SELECT ST_AsText(ST_GeomFromGeoJSON('{gp2}')) As geom)\

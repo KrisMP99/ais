01f0aed349b4b5a63477aceb7b81963d39f7370f
Cecilie <cfog19@student.aau.dk>
2022-03-24 20:42:34 +0100

    updated double quotes to single quotes

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,8 +22,8 @@ middleware = [
         CORSMiddleware, 
         allow_origins=['*'],
         allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"]
+        allow_methods=['*'],
+        allow_headers=['*']
     )
 ]
 

87c1d4d2af382a0234f8e0101d35dcd8611a5f78
Cecilie <cfog19@student.aau.dk>
2022-03-24 20:29:23 +0100

    updated fetch to run on 0.0.0.0

--- frontend/src/Components/PostButton.tsx
+++ frontend/src/Components/PostButton.tsx
@@ -28,7 +28,7 @@ async function postCoordinates(coordinates: LatLng[]){
         })
     };
 
-    fetch('http://127.0.0.1:8000/trips/trip', requestOptions)
+    fetch('http://0.0.0.0:8080/trips/trip', requestOptions)
     .then(response => response.json())
     .then(data => console.log(data))
   };

0b7e2d1900da12dc986e77f1a4e3e922d9448e39
Kristian <kpede19@student.aau.dk>
2022-03-27 18:19:00 +0200

    WIP

--- backend/app/ais_data_handling/data_insertion.py
+++ backend/app/ais_data_handling/data_insertion.py
@@ -6,7 +6,7 @@ import psycopg2
 import os
 import pygrametl
 from pygrametl.datasources import SQLSource
-from pygrametl.tables import CachedDimension, FactTable
+from pygrametl.tables import CachedDimension, BatchFactTable
 from sqlalchemy import create_engine
 import datetime
 
@@ -77,48 +77,65 @@ def insert_into_star(logger):
     date_dim = CachedDimension(
         name='date_dim',
         key='date_id',
-        attributes=['date']
+        attributes=['date'],
+        lookupatts=['date'],
+        cacheoninsert=True,
     )
 
     nav_dim = CachedDimension(
         name='nav_dim',
         key='nav_id',
-        attributes=['navigational_status']
+        attributes=['navigational_status'],
+        lookupatts=['navigational_status'],
+        cacheoninsert=True,
+        prefill=True
     )
 
     ship_dim = CachedDimension(
         name='ship_dim',
         key='ship_id',
-        attributes=['mmsi','type_of_mobile','imo','name','callsign','type_of_position_fixing_device','width','length']
+        attributes=['mmsi','type_of_mobile','imo','name','callsign','type_of_position_fixing_device','width','length'],
+        lookupatts=['mmsi'],
+        cacheoninsert=True,
+        prefill=True
     )
 
     ship_type_dim = CachedDimension(
         name='ship_type_dim',
         key='ship_type_id',
-        attributes=['ship_type']
+        attributes=['ship_type'],
+        lookupatts=['ship_type'],
+        cacheoninsert=True,
+        prefill=True
     )
 
     time_dim = CachedDimension(
         name='time_dim',
         key='time_id',
-        attributes=['time']
+        attributes=['time'],
+        lookupatts=['time'],
+        cacheoninsert=True
     )
 
     trip_dim = CachedDimension (
         name='trip_dim',
         key='trip_id',
-        attributes=['line_string']
+        attributes=['line_string'],
+        lookupatts=['line_string']
     )
 
-    fact_table = FactTable(
+    fact_table = BatchFactTable(
         name='data_fact',
         keyrefs=['date_id','time_id','ship_type_id','ship_id','nav_id','trip_id'],
         measures=['location','rot','sog','cog','heading','draught','destination'],
+        batchsize=500000,
         targetconnection=conn_wrapper
     )
 
     logger.info("Inserting rows into star schema")
     index = 0
+    time_begin = datetime.datetime.now()
+    print("Time begin: " + time_begin.strftime("%d-%m-%Y, %H:%M:%S"))
     for row in ais_source:
         convert_timestamp_to_time_and_date(row)
         row['line_string'] = None
@@ -143,6 +160,10 @@ def insert_into_star(logger):
 
         fact_table.insert(row)
 
+    time_end = datetime.datetime.now()
+    time_delta = time_end - time_begin
+    print("Time end: " + time_end.strftime("%d%m%Y, %H:%M%S"))
+    print(f"Took approx: {time_delta.total_seconds() / 60} minutes")
     logger.info("Done inserting into star schema")
     logger.info("Generating line strings...")
 

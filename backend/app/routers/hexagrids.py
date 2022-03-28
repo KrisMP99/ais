bd3fbfc78df21cf21d28565725506611903390b5
Cecilie <cfog19@student.aau.dk>
2022-03-28 08:38:14 +0200

    wip

--- backend/app/routers/hexagrids.py
+++ backend/app/routers/hexagrids.py
@@ -2,9 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from fastapi.encoders import jsonable_encoder
 from app.dependencies import get_token_header
 from app.db.database import engine, Session
-from app.models.polygon import Polygon
 from app.models.coordinate import Coordinate
-#from geojson import Polygon
+from geojson import Polygon
 import asyncio
 import pandas as pd
 
@@ -19,27 +18,17 @@ router = APIRouter(
 @router.put('/index_hexagrid')
 async def index_hexagrid():
     query = "SELECT ST_AsGeoJSON(h.geom)::json FROM hexagrid AS h"
-    polygon_collection = []
+    polygon_coordinates = []
     for chunk in pd.read_sql_query(query , engine, chunksize=50000):  
         for json in chunk['st_asgeojson']:
             json['PK'] = {'row': 1, 'column': 2}  
-            polygon = Polygon(
-                p1=Coordinate(long=json['coordinates'][0][0][0], lat=json['coordinates'][0][0][1]),
-                p2=Coordinate(long=json['coordinates'][0][1][0], lat=json['coordinates'][0][1][1]),
-                p3=Coordinate(long=json['coordinates'][0][2][0], lat=json['coordinates'][0][2][1]),
-                p4=Coordinate(long=json['coordinates'][0][3][0], lat=json['coordinates'][0][3][1]),
-                p5=Coordinate(long=json['coordinates'][0][4][0], lat=json['coordinates'][0][4][1]),
-                p6=Coordinate(long=json['coordinates'][0][5][0], lat=json['coordinates'][0][5][1]),
-                p7=Coordinate(long=json['coordinates'][0][6][0], lat=json['coordinates'][0][6][1])
-            )
-            polygon_collection.append(polygon) 
-    
+            polygon_coordinates.append(json['coordinates'][0])
+    print(polygon_coordinates[0])
     print('Got all hexagons as json from database')
     print('Began sorting...')
-
-    polygon_collection.sort(key=lambda x: x.get('')))
-
-    return jsonable_encoder(polygon_collection[0])
+    
+    sorted_coordinates = sorted(polygon_coordinates)
+    return jsonable_encoder(sorted_coordinates[1], sorted_coordinates[2], sorted_coordinates[3])
 
 @router.get("/hexa_grid")
 async def get_map_bounds():

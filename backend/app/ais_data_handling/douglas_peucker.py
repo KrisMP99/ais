196b63e752a5728045778344a43565df94a3a455
Kristian <kpede19@student.aau.dk>
2022-03-28 16:22:56 +0200

    Fixed GDAL version + some testing

--- backend/app/ais_data_handling/douglas_peucker.py
+++ backend/app/ais_data_handling/douglas_peucker.py
@@ -2,14 +2,19 @@ from venv import create
 import pandas as pd
 from shapely.geometry import LineString
 import geopandas as gpd
+import datetime
 
 def create_line_strings(point_df, logger):
     # COLUMNS = ['timestamp', 'type_of_mobile', 'mmsi', 'latitude', 'longitude', 'navigational_status', 'rot', 'sog', 'cog', 'heading', 'imo', 'callsign', 'name', 'ship_type', 'width', 'length', 'type_of_position_fixing_device', 'draught', 'destination', 'trip_id', 'simplified_trip_id']
     logger.info("Creating line strings")
+    time_begin = datetime.datetime.now()
     line_string_df = gpd.GeoDataFrame(point_df, geometry=gpd.points_from_xy(point_df.latitude, point_df.longitude))
     line_string_df = line_string_df.groupby('trip_id')['geometry'].apply(lambda x: LineString(x.tolist()))
+    time_end = datetime.datetime.now()
+    time_delta = time_end - time_begin
     
     print(line_string_df.head(5))
+    print(f"Took approx: {time_delta.total_seconds() / 60} minutes, or {time_delta.total_seconds()} seconds.")
     quit()
 
     # simplified_line_strings_df = point_df.groupby('trip_id').apply(lambda x: x. )

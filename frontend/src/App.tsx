d3d4132f9391720f6e662bbef6861ddf4538dd73
Jonas Noermark <60742511+jnoerm19@users.noreply.github.com>
2022-03-29 12:20:45 +0200

    merge complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -38,7 +38,7 @@ export class App extends React.Component<any, AppStates> {
             retCoords={(points: LatLng[]) => {
               this.setState({ pointCoords: points });
             }}
-            retMousePos={(pos: string[]) => { this.setState({mouseCoords: pos}); }}
+            retMousePos={(pos: string[]) => { this.setState({ mouseCoords: pos }); }}
             polylines={this.state.polylines}
           />
           <div className='right-side'>
@@ -46,21 +46,21 @@ export class App extends React.Component<any, AppStates> {
               <p className='text-1'>Positions:</p>
               <div>
                 <p className='text-2'>
-                  Current mouse location:<br/>
+                  Current mouse location:<br />
                   Lat: {this.textIsNotUndefined(0, true)} Lng: {this.textIsNotUndefined(0, false)}
                 </p>
                 <p className='text-2'>
-                  Point 1:<br/>
+                  Point 1:<br />
                   Lat: {this.textIsNotUndefined(1, true)} Lng: {this.textIsNotUndefined(1, false)}
                 </p>
                 <p className='text-2'>
-                  Point 2:<br/>
+                  Point 2:<br />
                   Lat: {this.textIsNotUndefined(2, true)} Lng: {this.textIsNotUndefined(2, false)}
                 </p>
               </div>
               <PostButton
                 coordinates={this.state.pointCoords}
-                getData={(data: LatLng[]) => this.setState({polylines: data})}
+                getData={(data: LatLng[]) => this.setState({ polylines: data })}
               />
             </div>
             <div className='filter-container'>
@@ -72,13 +72,13 @@ export class App extends React.Component<any, AppStates> {
   }
 
   protected textIsNotUndefined(index: number, lat: boolean): string {
-    if(this.state.mouseCoords && index == 0) {
+    if (this.state.mouseCoords && index == 0) {
       return lat ? this.state.mouseCoords[0] : this.state.mouseCoords[1];
     }
-    if(this.state.pointCoords.length >= 1 && index == 1) {
+    if (this.state.pointCoords.length >= 1 && index == 1) {
       return lat ? this.state.pointCoords[0].lat.toFixed(4) : this.state.pointCoords[0].lng.toFixed(4);
     }
-    else if(this.state.pointCoords.length == 2 && index == 2) {
+    else if (this.state.pointCoords.length == 2 && index == 2) {
       return lat ? this.state.pointCoords[1].lat.toFixed(4) : this.state.pointCoords[1].lng.toFixed(4);
     }
     return "0";

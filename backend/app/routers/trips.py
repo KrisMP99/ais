483993b2b498eece344f788a083e42d98b9c97ce
Cecilie <cfog19@student.aau.dk>
2022-04-01 14:42:32 +0200

    test

--- backend/app/routers/trips.py
+++ backend/app/routers/trips.py
@@ -111,7 +111,7 @@ async def get_trip(p1: Coordinate, p2: Coordinate):
                                         ST_Intersects(                                                          \
                                             ST_FlipCoordinates(std.line_string),                                \
                                             ST_SetSRID(hex2.geom, 3857)                                         \
-                                        )                                                                       \
+                                        ))                                                                      \
                                                                                                                 \
                                 SELECT                                                                          \
                                     CASE                                                                        \

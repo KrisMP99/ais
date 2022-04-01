3ae42af319f97fcfaf5e738d12be9fbc16d9d730
Cecilie <cfog19@student.aau.dk>
2022-04-01 09:35:25 +0200

    updated linstring query

--- backend/app/routers/trips.py
+++ backend/app/routers/trips.py
@@ -44,7 +44,7 @@ async def get_trip(p1: Coordinate, p2: Coordinate):
     SELECT ST_AsGeoJSON(l.geom)::json AS st_asgeojson\
     FROM simplified_trip_dim as std, gp1, gp2\
     WHERE ST_Intersects(ST_FlipCoordinates(std.line_string), ST_SetSRID(gp1.geom, 3857))\
-    AND ST_Intersects(ST_FlipCoordinates(std.line_string, ST_SetSRID(gp2.geom, 3857));"
+    AND ST_Intersects(ST_FlipCoordinates(std.line_string), ST_SetSRID(gp2.geom, 3857));"
     #linestring_query = "SELECT ST_AsGeoJSON(td.line_string)::json AS st_asgeojson FROM simplified_trip_dim AS td"
 
     linestrings = []

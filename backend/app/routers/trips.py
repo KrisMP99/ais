0d63b22c6233098723d22c481b2fe5f7757a8727
Cecilie <cfog19@student.aau.dk>
2022-04-01 16:26:44 +0200

    readded null, inserted st-within

--- backend/app/routers/trips.py
+++ backend/app/routers/trips.py
@@ -129,16 +129,20 @@ async def get_trip(p1: Coordinate, p2: Coordinate):
                                                 WHERE                                                           \
                                                     std.simplified_trip_id = data_fact.simplified_trip_id AND   \
                                                     std.simplified_trip_id = pil.simplified_trip_id AND         \
-                                                    time_dim.time IS NOT NULL                                  \
-                                                LIMIT 1)                                                             \
-                                    END AS geom, time_dim.time, data_fact.sog                                                  \
+                                                    time_dim.time IS NOT NULL                                   \
+                                                LIMIT 1)                                                        \
+                                        ELSE null                                                               \
+                                    END AS geom, time_dim.time, data_fact.sog                                   \
                                 FROM                                                                            \
                                     points_in_linestring AS pil, hex1, hex2, data_fact, date_dim, time_dim      \
                                 WHERE                                                                           \
                                     pil.simplified_trip_id = data_fact.simplified_trip_id AND                   \
                                     data_fact.date_id = date_dim.date_id AND                                    \
                                     data_fact.time_id = time_dim.time_id AND                                    \
-                                    pil.geom = data_fact.location"
+                                    pil.geom = data_fact.location AND                                           \
+                                    ST_Within(                                                                  \
+                                                ST_FlipCoordinates(pil.geom),                                   \
+                                                ST_SetSRID(hex1.geom, 3857)) "
 
     for chunk in pd.read_sql_query(linestring_query_hexagon, engine, chunksize=50000):
         if len(chunk) != 0:

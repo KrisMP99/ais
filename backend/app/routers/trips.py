11b5eaf4a8d8a129cbcb470c2b2938ed1cef6d3d
Cecilie <cfog19@student.aau.dk>
2022-04-01 15:15:44 +0200

    test

--- backend/app/routers/trips.py
+++ backend/app/routers/trips.py
@@ -140,8 +140,7 @@ async def get_trip(p1: Coordinate, p2: Coordinate):
 
     for chunk in pd.read_sql_query(linestring_query_hexagon, engine, chunksize=50000):
         if len(chunk) != 0:
-            for time in chunk:
-                print(time)
+            print(chunk)
         else:
             logger.warning('No trips were found for the selected coordinates')
             raise HTTPException(status_code=204, detail='No trips were found for the selected coordinates')

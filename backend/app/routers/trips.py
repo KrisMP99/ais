f30eca472d7b87b57b3b169e19202fa73a945b2d
Cecilie <cfog19@student.aau.dk>
2022-04-01 12:51:49 +0200

    test

--- backend/app/routers/trips.py
+++ backend/app/routers/trips.py
@@ -64,17 +64,24 @@ async def get_trip(p1: Coordinate, p2: Coordinate):
     linestring_query_hexagon = f"WITH gp1 AS (\
     SELECT ST_AsText(ST_GeomFromGeoJSON('{polygons[0]}')) As geom), \
     gp2 AS (SELECT ST_AsText(ST_GeomFromGeoJSON('{polygons[1]}')) As geom), \
-    points_in_linestring AS (SELECT ST_PointN(std.line_string, generate_series(1, ST_NPOINTS(std.line_string))) AS geom FROM simplified_trip_dim AS std, gp1, gp2\
+    points_in_linestring AS (SELECT ST_PointN(std.line_string, generate_series(1, ST_NPOINTS(std.line_string))) AS geom, std.simplified_trip_id FROM simplified_trip_dim AS std, gp1, gp2\
     WHERE ST_Intersects(ST_FlipCoordinates(std.line_string), ST_SetSRID(gp1.geom, 3857)) \
     AND ST_Intersects(ST_FlipCoordinates(std.line_string), ST_SetSRID(gp2.geom, 3857))) \
     \
     SELECT CASE \
         WHEN ST_Within(ST_FlipCoordinates(pil.geom), ST_SetSRID(gp1.geom, 3857)) \
-            THEN true \
+            THEN (SELECT * FROM simplified_trip_dim AS std, data_fact, date_dim, time_dim\
+            WHERE std.simplified_trip_id = data_fact.simplified_trip_id\
+            AND std.simplified_trip_id = pil.simplified_trip_id) \
+        ELSE null \
+    END first_point,\
+    CASE \
         WHEN ST_Within(ST_FlipCoordinates(pil.geom), ST_SetSRID(gp2.geom, 3857)) \
-            THEN true \
-        ELSE ST_Within(ST_FlipCoordinates(pil.geom), ST_SetSRID(gp1.geom, 3857)) \
-    END first_point\
+            THEN (SELECT * FROM simplified_trip_dim AS std, data_fact, date_dim, time_dim\
+            WHERE std.simplified_trip_id = data_fact.simplified_trip_id\
+            AND std.simplified_trip_id = pil.simplified_trip_id) \
+        ELSE null \
+    END second_point\
     FROM points_in_linestring AS pil, gp1, gp2;"
 
     #     CASE \

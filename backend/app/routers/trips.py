8d884b98d3886a4e76e2b263941f421548f8c898
Cecilie <cfog19@student.aau.dk>
2022-04-01 10:00:54 +0200

    test

--- backend/app/routers/trips.py
+++ backend/app/routers/trips.py
@@ -72,7 +72,9 @@ async def get_trip(p1: Coordinate, p2: Coordinate):
 
     for chunk in pd.read_sql_query(linestring_query_hexagon, engine, chunksize=50000):
         if len(chunk) != 0:
-            print(chunk)
+            for json in chunk['st_asgeojson']:
+                if json is not None:
+                    print(json['coordinates'])
         else:
             logger.warning('No trips were found for the selected coordinates')
             raise HTTPException(status_code=404, detail='No trips were found for the selected coordinates')

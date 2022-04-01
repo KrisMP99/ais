2417a3875929a072c5ddb49807d2ff60329fdee6
Cecilie <cfog19@student.aau.dk>
2022-04-01 12:30:00 +0200

    test

--- backend/app/routers/trips.py
+++ backend/app/routers/trips.py
@@ -69,11 +69,11 @@ async def get_trip(p1: Coordinate, p2: Coordinate):
     AND ST_Intersects(std.line_string, ST_SetSRID(gp2.geom, 3857))) \
     \
     SELECT CASE \
-        WHEN ST_Contains(pil.geom, ST_SetSRID(gp1.geom, 3857)) \
+        WHEN ST_Within(pil.geom, ST_SetSRID(gp1.geom, 3857)) \
             THEN true \
-        WHEN ST_Contains(pil.geom, ST_SetSRID(gp2.geom, 3857)) \
+        WHEN ST_Within(pil.geom, ST_SetSRID(gp2.geom, 3857)) \
             THEN true \
-        ELSE ST_Contains(pil.geom, ST_SetSRID(gp1.geom, 3857)) \
+        ELSE ST_Within(pil.geom, ST_SetSRID(gp1.geom, 3857)) \
     END first_point\
     FROM points_in_linestring AS pil, gp1, gp2;"
 

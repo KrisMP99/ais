1965bb39600c7b078e18b33a1caf7d87c0e8dd28
Jonas Noermark <60742511+jnoerm19@users.noreply.github.com>
2022-04-05 15:02:39 +0200

    WIP

--- backend/app/routers/trips.py
+++ backend/app/routers/trips.py
@@ -174,6 +174,11 @@ async def get_trip(p1: Coordinate, p2: Coordinate):
         latest_time_in_hex1 = hex1_df.time_id.iat[-1]
         latest_time_in_hex2 = hex2_df.time_id.iat[-1]
 
+        print("hex1:", hex1_df['time_id'])
+        print("hex2:", hex2_df['time_id'])
+
+        print("hex1:",latest_time_in_hex1, " hex2:", latest_time_in_hex2)
+
         if pointsInHex1 > pointsInHex2:
             if latest_time_in_hex1 > latest_time_in_hex2:
                 hex1_df = hex1_df[(df['time_id'] < latest_time_in_hex2)]

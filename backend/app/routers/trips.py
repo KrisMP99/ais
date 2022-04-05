35973645debb5dad3e4c0d560f63a4dc272ea8d8
Jonas Noermark <60742511+jnoerm19@users.noreply.github.com>
2022-04-05 15:06:29 +0200

    WIP

--- backend/app/routers/trips.py
+++ backend/app/routers/trips.py
@@ -176,14 +176,14 @@ async def get_trip(p1: Coordinate, p2: Coordinate):
 
         if pointsInHex1 > pointsInHex2:
             if latest_time_in_hex1 > latest_time_in_hex2:
-                hex1_df = hex1_df[(df['time_id'] > latest_time_in_hex2)]
+                hex1_df = hex1_df[(hex1_df['time_id'] > latest_time_in_hex2)]
             else:
-                hex1_df = hex1_df[(df['time_id'] < latest_time_in_hex2)]
+                hex1_df = hex1_df[(hex1_df['time_id'] < latest_time_in_hex2)]
         else:
             if(latest_time_in_hex2 > latest_time_in_hex1):
-                hex2_df = hex2_df[(df['time_id'] > latest_time_in_hex1)]
+                hex2_df = hex2_df[(hex2_df['time_id'] > latest_time_in_hex1)]
             else:
-                hex2_df = hex2_df[(df['time_id'] < latest_time_in_hex1)]
+                hex2_df = hex2_df[(hex2_df['time_id'] < latest_time_in_hex1)]
             
     print("Hex_df1: ", hex1_df.head())
     print("Hex_df2: ", hex2_df.head())

fcbe591bd67aba156e91448940327f7c914f9522
Jonas Noermark <60742511+jnoerm19@users.noreply.github.com>
2022-04-05 14:19:45 +0200

    wip

--- backend/app/routers/trips.py
+++ backend/app/routers/trips.py
@@ -161,8 +161,8 @@ async def get_trip(p1: Coordinate, p2: Coordinate):
     loop = asyncio.get_event_loop()
     df = await loop.run_in_executor(None, pd.read_sql_query, point_exists_in_hexagon_query, engine)
     hexagons_list = df['hexgeom'].unique().tolist()
-    hex1_df = df[(df['hexgeom'] == df[hexagons_list[0]])]
-    hex2_df = df[(df['hexgeom'] == df[hexagons_list[1]])]
+    hex1_df = df[(df['hexgeom'] == df[str(hexagons_list[0])])]
+    hex2_df = df[(df['hexgeom'] == df[str(hexagons_list[1])])]
 
     print("Hex_df1: ", hex1_df.head())
     print("Hex_df2: ", hex2_df.head())

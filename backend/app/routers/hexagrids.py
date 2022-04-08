820c7955c3cf55cfc9be3e7761d9bacc02db6184
Cecilie <cfog19@student.aau.dk>
2022-04-08 13:54:31 +0200

    added params to gethex

--- backend/app/routers/hexagrids.py
+++ backend/app/routers/hexagrids.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from app.dependencies import get_token_header, get_logger
 from app.db.database import engine, Session
 from app.models.coordinate import Coordinate
-from geojson import Point
+from shapely.geometry.point import Point
 import asyncio
 import pandas as pd
 
@@ -17,26 +17,19 @@ router = APIRouter(
 
 @router.post('/hexagon')
 async def get_hexagon(p1: Coordinate):
-    gp1 = Point((p1.long, p1.lat))
-    query = f"WITH gp1 AS (                                         \
-            SELECT                                                  \
-                ST_AsText(                                          \
-                    ST_GeomFromGeoJSON('{gp1}')) As geom            \
-            )                                                       \
-                                                                    \
-            SELECT                                                  \
-                ST_AsGeoJSON(                                       \
-                    ST_FlipCoordinates(h.geom)                      \
-                )::json AS st_asgeojson                             \
-            FROM                                                    \
-                hexagrid AS h, gp1                                  \
-            WHERE                                                   \
-                ST_Intersects(                                      \
-                    h.geom, ST_SetSRID(gp1.geom, 3857)              \
-                );"
+    geomPoint = Point(p1.long, p1.lat)
+    query = """SELECT                                                   \
+                ST_AsGeoJSON(                                           \
+                    ST_FlipCoordinates(h.geom)                          \
+                )::json AS st_asgeojson                                 \
+            FROM                                                        \
+                hexagrid AS h                                           \
+            WHERE                                                       \
+                ST_Intersects(                                          \
+                    h.geom, ST_GeomFromWKB(%(geom)s::geometry, 3857)    \
+                );"""
 
-    loop = asyncio.get_event_loop()
-    result = await loop.run_in_executor(None, pd.read_sql_query, query, engine)
+    result = pd.read_sql_query(query, engine, params={'geom': geomPoint.wkb_hex})
 
     if len(result) == 0:
         logger.error('Could not find the given coordinates')

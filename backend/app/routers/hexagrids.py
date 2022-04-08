d9cddb9a98d112ed529d7592a1a5039b7548b7a2
Cecilie <cfog19@student.aau.dk>
2022-04-08 11:36:46 +0200

    deleted latest changes

--- backend/app/routers/hexagrids.py
+++ backend/app/routers/hexagrids.py
@@ -3,7 +3,6 @@ from app.dependencies import get_token_header, get_logger
 from app.db.database import engine, Session
 from app.models.coordinate import Coordinate
 from geojson import Point
-from shapely.geometry import Polygon
 import asyncio
 import pandas as pd
 
@@ -19,40 +18,30 @@ router = APIRouter(
 @router.post('/hexagon')
 async def get_hexagon(p1: Coordinate):
     gp1 = Point((p1.long, p1.lat))
-    query = f"WITH gp1 AS (                                                             \
-                SELECT ST_AsText(                                                       \
-                    ST_GeomFromGeoJSON('{gp1}')                                         \
-                ) As geom                                                               \
-            )                                                                           \
-                                                                                        \
-            SELECT                                                                      \
-                ST_AsGeoJSON(                                                           \
-                    ST_FlipCoordinates(h.geom)                                          \
-                )::json AS st_asgeojson, h.hid                                          \
-            FROM                                                                        \
-                hexagrid as h, gp1                                                      \
-            WHERE                                                                       \
-                ST_Intersects(                                                          \
-                    h.geom, ST_SetSRID(gp1.geom, 3857)                                  \
+    query = f"WITH gp1 AS (                                         \
+            SELECT                                                  \
+                ST_AsText(                                          \
+                    ST_GeomFromGeoJSON('{gp1}')) As geom            \
+            )                                                       \
+                                                                    \
+            SELECT                                                  \
+                ST_AsGeoJSON(                                       \
+                    ST_FlipCoordinates(h.geom)                      \
+                )::json AS st_asgeojson                             \
+            FROM                                                    \
+                hexagrid AS h, gp1                                  \
+            WHERE                                                   \
+                ST_Intersects(                                      \
+                    h.geom, ST_SetSRID(gp1.geom, 3857)              \
                 );"
 
-    #     query = f"WITH gp1 AS (\
-    # SELECT ST_AsText(ST_GeomFromGeoJSON('{gp1}')) As geom)\
-    # SELECT ST_AsGeoJSON(ST_FlipCoordinates(h.geom))::json AS st_asgeojson\
-    # FROM hexagrid as h, gp1\
-    # WHERE ST_Intersects(h.geom, ST_SetSRID(gp1.geom, 3857));"
-
     loop = asyncio.get_event_loop()
-    df = await loop.run_in_executor(None, pd.read_sql_query, query, engine)
+    result = await loop.run_in_executor(None, pd.read_sql_query, query, engine)
 
-    if len(df) == 0:
+    if len(result) == 0:
         logger.error('Could not find the given coordinates')
         raise HTTPException(status_code=404, detail='Could not find the given coordinates')
-    
-    polygon = {
-        'hid': df['hid'].to_list()[0],
-        'geom': df['st_asgeojson'][0]['coordinates'][0]
-    } 
+    polygon = result['st_asgeojson'][0]['coordinates'][0]
 
     return polygon
 

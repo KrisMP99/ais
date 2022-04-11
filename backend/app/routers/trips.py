eca7a17d66ce9d8a0c0a837674540ace9489ac8f
Cecilie <cfog19@student.aau.dk>
2022-04-11 09:49:50 +0200

    new test

--- backend/app/routers/trips.py
+++ backend/app/routers/trips.py
@@ -232,8 +232,8 @@ def create_point(hexagon: Hexagon, linestring: str, hexagons: list[Hexagon]):
                     SELECT 
                         DISTINCT pil.geom
                     FROM
-                        points_in_linestring AS pil;
-                    WHERE ST_ClosestPoint(%(hex)s::geometry, pil.geom)
+                        points_in_linestring AS pil
+                    WHERE ST_ClosestPoint(%(hex)s::geometry, pil.geom);
                 '''
     # Get the closest point to the hexagon, so we can use the data from that point.            
     # Calculate the distance from ST_ClosestPoint(ST_Centroid(hexagon), linestring) to the point found before.

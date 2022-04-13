7d506c717a111a1156653dece63dd41fde40e492
Cecilie <cfog19@student.aau.dk>
2022-04-13 20:23:43 +0200

    updated row and column in model

--- backend/app/models/hexagon.py
+++ backend/app/models/hexagon.py
@@ -1,6 +1,7 @@
 from shapely.geometry import Polygon
 
 class Hexagon:
-    def __init__(self, hid:int, geom:Polygon):
-        self.hid = hid
+    def __init__(self, column:int, row:int, geom:Polygon):
+        self.column = column
+        self.row = row
         self.geom = geom

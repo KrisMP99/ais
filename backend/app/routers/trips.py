656ee9ea7bbafeeb164698b6c94655a0c2fb2d5f
Cecilie <cfog19@student.aau.dk>
2022-04-19 12:09:02 +0200

    test

--- backend/app/routers/trips.py
+++ backend/app/routers/trips.py
@@ -69,7 +69,7 @@ async def get_trip(p1: Coordinate, p2: Coordinate):
         locations = []
         for location in line_string.locations:
             location:Location
-            locations.append(list(transform(flip, location.location.coords)))
+            locations.append(list(transform(flip, location.location).coords))
 
         line_string_to_return_to_frontend.append(locations)
 
